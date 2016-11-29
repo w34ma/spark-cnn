6033e9c463eae21d12a501ad2b2be6eae664376b
Vivi <w34ma23@gmail.com>
2016-11-28 23:28:30 -0500

    return None for now

--- cnn/pool.py
+++ cnn/pool.py
@@ -7,7 +7,9 @@ class PoolingLayer():
         # input X activation matrix from conv layer [N x W x H x D]
         # where D is the depth, also the number of filters from conv layer
         # output max pooled activation matrix [N x W_ x H_ x D]
+        return None
     def backward(self, df, X):
         # input df are gradients from upstream [N x W_ x H_ x D]
         # input X is the activation matrix taken in at forward run [N x W x H x D]
         # output dX gradients on X [N x W x H x D]
+        return None
