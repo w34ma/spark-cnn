76eb3601f58bb5d9eb1164148c5f5397d1cd4ad6
Vivi <w34ma23@gmail.com>
2016-11-29 22:52:37 -0500

    working on spark

---
+++ cnn/spark_cnn.py
@@ -0,0 +1,24 @@
+# spark enabled CNN
+
+import numpy as np
+from time import time
+from conv import ConvolutionLayer
+from relu import ReLULayer
+from pool import PoolingLayer
+from fc import FCLayer
+from utils import *
+
+from cnn import CNN
+
+class SparkCNN(CNN)
+    def __init__(self, I, B):
+        CNN.__init__(self, I)
+        self.B = B # number of batches
+
+    """ override train with spark """
+    def train(self, size = 1000):
+        classifications = load_classifications()
+        C = len(classifications)
+        self.init_layers(C)
+
+        print('Start training CNN with Spark')
