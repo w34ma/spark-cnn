8a48c36095b5f50eeffde2cdbd41bf3bfa4c043a
Vivi <w34ma23@gmail.com>
2016-12-02 03:18:52 -0500

    good progress

--- cnn/compare_cnn.py
+++ cnn/compare_cnn.py
@@ -7,7 +7,7 @@ from fc import FCLayer
 from utils import *
 from time import time
 
-N = 2000
+N = 1000
 W = 32
 H = 32
 D = 3
--- cnn/compare_spark.py
+++ cnn/compare_spark.py
@@ -1,3 +1,5 @@
+from pyspark.sql import SparkSession
+
 import numpy as np
 from cnn import CNN
 from conv import ConvolutionLayer
@@ -7,7 +9,7 @@ from fc import FCLayer
 from utils import *
 from time import time
 
-N = 2000
+N = 1000
 W = 32
 H = 32
 D = 3
@@ -28,74 +30,70 @@ RS = cnn.forward(X)
 R1, R2, R3, R4 = RS
 L, dS = softmax(R4, Y)
 
+"""
+# non spark
+non_spark_start = time()
+# fc
+dXFC_1, dAFC_1, dbFC_1 = cnn.fc.backward(dS, R3)
+
+# pool
+dXPool_1 = cnn.pool.backward(dXFC_1, R2)
+
+# ReLU
+dXReLU_1 = cnn.relu.backward(dXPool_1, R1)
+
+# conv
+dXConv_1, dAConv_1, dbConv_1 = cnn.conv.backward(dXReLU_1, X)
+non_spark_end = time()
+print('Non Spark: %.3f' % (non_spark_end - non_spark_start))
+"""
+
 spark_start = time()
 
-dXFC_2 = []
-dAFC_2 = []
-dbFC_2 = []
-dXPool_2 = []
-dXReLU_2 = []
-dXConv_2 = []
-dAConv_2 = []
-dbConv_2 = []
+spark = SparkSession.builder.appName('cnn').getOrCreate()
+sc = spark.sparkContext
+
 
 step = 100
+batches = N // step
 
-for i in range(0, N // step):
-    dS_i = dS[step * i : step * i + step, :].reshape(step, C)
-    # R3_i = R3[step * i : step * i + step, :, :, :].reshape(step, W // 2, N // 2, K)
-    R3_i = R3[step * i : step * i + step, :, :, :]
-    R2_i = R2[step * i : step * i + step, :, :, :]
-    R1_i = R1[step * i : step * i + step, :, :, :]
-    X_i = X[step * i : step * i + step, :, :, :]
-    # fc
-    dXFC_i, dAFC_i, dbFC_i = cnn.fc.backward(dS_i, R3_i)
-    dXFC_2.append(dXFC_i)
-    dAFC_2.append(dAFC_i)
-    dbFC_2.append(dbFC_i)
+def calculation(b):
+    start = b * step
+    end = start + step
+    dS_b = dS[start:end, :]
+    R3_b = R3[start:end, :, :, :]
+    R2_b = R2[start:end, :, :, :]
+    R1_b = R1[start:end, :, :, :]
+    X_b = X[start:end, :, :, :]
 
-    # pool
-    dXPool_i = cnn.pool.backward(dXFC_i, R2_i)
-    dXPool_2.append(dXPool_i)
+    dXFC_b, dAFC_b, dbFC_b = cnn.fc.backward(dS_b, R3_b)
+    dXPool_b = cnn.pool.backward(dXFC_b, R2_b)
+    dXReLU_b = cnn.relu.backward(dXPool_b, R1_b)
+    dXConv_b, dAConv_b, dbConv_b = cnn.conv.backward(dXReLU_b, X_b)
 
-    # relu
-    dXReLU_i = cnn.relu.backward(dXPool_i, R1_i)
-    dXReLU_2.append(dXReLU_i)
-
-    # conv
-    dXConv_i, dAConv_i, dbConv_i = cnn.conv.backward(dXReLU_i, X_i)
-    dXConv_2.append(dXConv_i)
-    dAConv_2.append(dAConv_i)
-    dbConv_2.append(dbConv_i)
+    return [dAFC_b, dbFC_b, dAConv_b, dbConv_b]
 
 
-dXFC_2 = np.concatenate(dXFC_2)
-dAFC_2 = np.sum(dAFC_2, 0)
-dbFC_2 = np.sum(dbFC_2, 0)
-
-dXPool_2 = np.concatenate(dXPool_2)
-
-dXReLU_2 = np.concatenate(dXReLU_2)
-
-dXConv_2 = np.concatenate(dXConv_2)
-dAConv_2 = np.sum(dAConv_2, 0)
-dbConv_2 = np.sum(dbConv_2, 0)
+R = sc.parallelize(range(0, batches)).map(calculation).collect()
+R = np.sum(np.asarray(R), 0)
+dAFC = R[0]
+dbFC = R[1]
+dAConv = R[2]
+dbConv = R[3]
+print(dAFC.shape)
+print(dbFC.shape)
+print(dAConv.shape)
+print(dbConv.shape)
 
 spark_end = time()
 print('Spark: %.3f' % (spark_end - spark_start))
 
+
 """
-assert np.allclose(dXFC_1, dXFC_2), 'dX failed'
-assert np.allclose(dAFC_1, dAFC_2), 'dAFC failed'
-assert np.allclose(dbFC_1, dbFC_2), 'dbFC failed'
-
-assert np.allclose(dXPool_1, dXPool_2), 'dXPool failed'
-
-assert np.allclose(dXReLU_1, dXReLU_2), 'dXReLU failed'
-
-assert np.allclose(dXConv_1, dXConv_2), 'dXConv failed'
-assert np.allclose(dAConv_1, dAConv_2), 'dAConv failed'
-assert np.allclose(dbConv_1, dbConv_2), 'dbConv failed'
+assert np.allclose(dAFC_1, dAFC), 'dAFC failed'
+assert np.allclose(dbFC_1, dbFC), 'dbFC failed'
+assert np.allclose(dAConv_1, dAConv), 'dAConv failed'
+assert np.allclose(dbConv_1, dbConv), 'dbConv failed'
 """
 
 print('done!')
