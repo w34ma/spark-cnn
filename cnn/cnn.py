fa0ae0f3e3dada7e04ba4399658be00889ab562c
Vivi Ma <w34ma23@gmail.com>
2016-12-14 19:20:29 -0500

    revised version

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -25,10 +25,10 @@ class CNN():
 
     def init_layers(self, C):
         # initialize layers
-        self.conv = ConvolutionLayer(64, 3, 1, 3, 1)
+        self.conv = ConvolutionLayer(32, 5, 1, 3, 2)
         self.relu = ReLULayer()
         self.pool = PoolingLayer(2, 2)
-        self.fc = FCLayer(16, 16, 64, C)
+        self.fc = FCLayer(16, 16, 32, C)
 
     def train(self, size = 1000):
         X, Y = load_training_data(0, size)
